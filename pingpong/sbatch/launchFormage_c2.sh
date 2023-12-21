a24aa4b1385c0d8827cc57b804dbd602541092ea
Lorenzo Pichetti <lori.pichi@gmail.com>
2023-12-21 17:37:07 +0100

    pingpong launchFormage and Makefile.MARZOLA

--- pingpong/sbatch/launchFormage_c2.sh
+++ pingpong/sbatch/launchFormage_c2.sh
@@ -11,6 +11,11 @@
 
 # Currently Loaded Modulefiles:
 #  1) slurm/slurm-01/23.02.6   2) hpcx   3) nvhpc-hpcx-cuda12/23.11   4) openmpi/4.1.5   5) binutils/12.2.0   6) gcc/12.2.0
+module unload gcc/13.1.0
+module load gcc/12.2.0
+module load openmpi/4.1.5
+module load nvhpc-hpcx-cuda12/23.11
+
 
 mainfolder=/home/lpichetti/MPI_GPU_banch/pingpong
 binfolder=${mainfolder}/bin
@@ -25,7 +30,7 @@ do
 	binaryFile=$(echo "$binaryPath" | sed 's|.*/||')
 	echo "binaryFile= --> $binaryFile"
 	start_time="$(date -u +%s)"
-	exitcode=$(mpirun -np 2 ${binaryPath} > ${outfolder}/${binaryFile}.out 2> ${outfolder}/${binaryFile}.err)
+	exitcode=$(mpirun -np 2 ${binaryPath} > ${outfolder}/${binaryFile}_c2.out 2> ${outfolder}/${binaryFile}_c2.err)
 	end_time="$(date -u +%s)"
 
 	echo " exitcode:    $exitcode"
--- pingpong/sbatch/launchFormage_cg1.sh
+++ pingpong/sbatch/launchFormage_cg1.sh
@@ -9,9 +9,12 @@
 #SBATCH --partition=cg1-cpu480gb-gpu96gb
 #SBATCH --exclusive
 
-# module load cuda/
-# module load nccl/
-# module load openmpi/
+# Currently Loaded Modulefiles:
+#  1) slurm/slurm-01/23.02.6   2) hpcx   3) nvhpc-hpcx-cuda12/23.11   4) openmpi/4.1.5   5) binutils/12.2.0   6) gcc/12.2.0
+module unload gcc/13.1.0
+module load gcc/12.2.0
+module load openmpi/4.1.5
+module load nvhpc-hpcx-cuda12/23.11
 
 mainfolder=/home/lpichetti/MPI_GPU_banch/pingpong
 binfolder=${mainfolder}/bin
@@ -26,7 +29,7 @@ do
 	binaryFile=$(echo "$binaryPath" | sed 's|.*/||')
 	echo "binaryFile= --> $binaryFile"
 	start_time="$(date -u +%s)"
-	exitcode=$(mpirun -np 2 ${binaryPath} > ${outfolder}/${binaryFile}.out 2> ${outfolder}/${binaryFile}.err)
+	exitcode=$(mpirun -np 2 ${binaryPath} > ${outfolder}/${binaryFile}_cg1.out 2> ${outfolder}/${binaryFile}_cg1.err)
 	end_time="$(date -u +%s)"
 
 	echo " exitcode:    $exitcode"
