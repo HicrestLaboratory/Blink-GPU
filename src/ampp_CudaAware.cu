cbe9d951083efff62f1946294a1ef04a879f68dd
Zebin Ren <zebinren97@gmail.com>
2024-03-12 14:41:54 +0200

    [Refactor] Move select device to separate header

--- src/ampp_CudaAware.cu
+++ src/ampp_CudaAware.cu
@@ -8,7 +8,7 @@
 #include "../include/error.h"
 #include "../include/type.h"
 #include "../include/gpu_ops.h"
-
+#include "../include/device_assignment.h"
 
 #if !defined(OPEN_MPI) || !OPEN_MPI
 #error This source code uses an Open MPI-specific extension
@@ -27,72 +27,6 @@
 static int stringCmp( const void *a, const void *b) {
      return strcmp((const char*)a,(const char*)b);
 
-}
-
-int  assignDeviceToProcess(MPI_Comm *nodeComm, int *nnodes, int *mynodeid)
-{
-#ifdef MPI
-      char     host_name[MPI_MAX_PROCESSOR_NAME];
-      char (*host_names)[MPI_MAX_PROCESSOR_NAME];
-
-#else
-      char     host_name[20];
-#endif
-      int myrank;
-      int gpu_per_node;
-      int n, namelen, color, rank, nprocs;
-      size_t bytes;
-/*
-      if (chkseGPU()<1 && 0) {
-        fprintf(stderr, "Invalid GPU Serial number\n");
-	exit(EXIT_FAILURE);
-      }
-*/
-
-#ifdef MPI
-      MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-      MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
-      MPI_Get_processor_name(host_name,&namelen);
-
-      bytes = nprocs * sizeof(char[MPI_MAX_PROCESSOR_NAME]);
-      host_names = (char (*)[MPI_MAX_PROCESSOR_NAME]) malloc(bytes);
-
-      strcpy(host_names[rank], host_name);
-
-      for (n=0; n<nprocs; n++)
-      {
-       MPI_Bcast(&(host_names[n]),MPI_MAX_PROCESSOR_NAME, MPI_CHAR,n, MPI_COMM_WORLD);
-      }
-
-
-      qsort(host_names, nprocs, sizeof(char[MPI_MAX_PROCESSOR_NAME]), stringCmp);
-
-      color = 0;
-
-      for (n=0; n<nprocs; n++)
-      {
-        if(n>0&&strcmp(host_names[n-1], host_names[n])) color++;
-        if(strcmp(host_name, host_names[n]) == 0) break;
-      }
-
-      MPI_Comm_split(MPI_COMM_WORLD, color, 0, nodeComm);
-
-      MPI_Comm_rank(*nodeComm, &myrank);
-      MPI_Comm_size(*nodeComm, &gpu_per_node);
-
-      MPI_Allreduce(&color, nnodes, 1, MPI_INT, MPI_MAX, MPI_COMM_WORLD);
-      (*mynodeid) = color;
-      (*nnodes) ++;
-
-#else
-     //*myrank = 0;
-     return 0;
-#endif
-
-//      printf ("Assigning device %d  to process on node %s rank %d\n",*myrank,  host_name, rank );
-      /* Assign device to MPI process, initialize BLAS and probe device properties */
-      //cudaSetDevice(*myrank);
-      return myrank;
 }
 
 // ---------------------------------------
