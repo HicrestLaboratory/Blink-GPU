ad40500e3bf8f3d70045db06c8b03ba2ceba4777
Daniele De Sensi <d.desensi.software@gmail.com>
2024-03-18 15:55:43 +0100

    [FIX] MPI_INT -> MPI_INT32_T.

--- include/type.h
+++ include/type.h
@@ -5,10 +5,10 @@
 #include <unistd.h>
 
 #define dtype u_int8_t
-#define MPI_dtype MPI_BYTE // MPI_UINT8_T?
+#define MPI_dtype MPI_BYTE 
 
 #define cktype int32_t
-#define MPI_cktype MPI_INT // MPI_INT32_T?
+#define MPI_cktype MPI_INT32_T
 
 #define ncclDtype ncclChar // ncclUint8
 
