f0f46c1e29b3033711169b560bc19d47a93d363b
Daniele De Sensi <desensi@di.uniroma1.it>
2024-03-19 21:16:27 +0200

    [IMP] Code translator.

--- create_hip_file.sh
+++ create_hip_file.sh
@@ -1,13 +1,21 @@
 #! /bin/bash
-names=("mpp" "a2a" "ar")
-types=("Baseline" "CudaAware" "Nccl") # "Nvlink")
+names=("mpp" "pp" "a2a" "ar" "otom")
+types=("Baseline" "CudaAware" "Nccl" "Nvlink")
 
-rm src/*.cpp
+#names=("mpp" "pp" "a2a")
+#types=("Nvlink")
 
 for name in "${names[@]}"
 do
     for type in "${types[@]}"
     do
+        # Combinations to skip
+        if [[ $name == "otom" && $type != "Nccl" ]]; then
+            continue
+        fi
+        if [[ $name == "ar" && $type == "Nvlink" ]]; then
+            continue
+        fi        
         cur_name=${name}_${type}
         cp src/${cur_name}.cu src/${cur_name}.cpp
         sed -i 's/#include <cuda.h>//g' src/${cur_name}.cpp
@@ -19,5 +27,11 @@ do
         sed -i 's/<nccl.h>/<rccl.h>/g' src/${cur_name}.cpp
         sed -i 's/hipHostAlloc(/hipHostMalloc((void **)/g' src/${cur_name}.cpp
         sed -i 's/hipHostAllocDefault/hipHostMallocDefault/g' src/${cur_name}.cpp
+        sed -i 's/hipDeviceProp/hipDeviceProp_t/g' src/${cur_name}.cpp
+        sed -i 's/hipFreeHost/hipHostFree/g' src/${cur_name}.cpp # hipFreeHost deprecated
+        sed -i 's/!prop.unifiedAddressing/0/g' src/${cur_name}.cpp # We need this because on old ROCM versions the unifiedAddressing property is not available
+        #if [[ $type == "Nvlink" ]]; then
+        #    sed -i 's/hipMalloc/hipMallocManaged/g' src/${cur_name}.cpp
+        #fi    
     done
 done
