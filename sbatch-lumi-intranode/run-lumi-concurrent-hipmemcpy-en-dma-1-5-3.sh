a35ceda8723f301172428b4d95ecf9faee3a2dd2
Zebin Ren <zebinren97@gmail.com>
2024-03-22 22:11:23 +0200

    [spport lumi] add intra-node sbatch

---
+++ sbatch-lumi-intranode/run-lumi-concurrent-hipmemcpy-en-dma-1-5-3.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#SBATCH --job-name=concurrent_hipmemcpy_en_dma_1_5_3
+#SBATCH --output=sout/lumi_concurrent_hipmemcpy_en_dma_1_5_3_%j.out
+#SBATCH --error=sout/lumi_concurrent_hipmemcpy_en_dma_1_5_3_%j.err
+
+#SBATCH --partition=ju-standard-g
+#SBATCH --time=00:10:00
+
+#SBATCH --nodes=1
+#SBATCH --gpus-per-node=8
+#SBATCH --ntasks-per-node=1
+#SBATCH --account=project_465000997
+
+# CPU_BIND="map_cpu:9,25,41,57,1,17,33,49"
+
+mkdir -p sout
+
+export HSA_ENABLE_SDMA=1
+
+module purge
+module load LUMI/23.09
+module load PrgEnv-cray
+module load craype-accel-amd-gfx90a
+module load rocm
+
+srun bin/concurrent_hipmemcpy -g0 1 -g1 5 -g2 3
