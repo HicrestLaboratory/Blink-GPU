0259628b019db85d1d02d9328fbfabe43b3814b3
Lorenzo Pichetti <lpichetti@int6.local.snellius.surf.nl>
2024-11-28 16:00:23 +0100

    fixes

--- include/energy/picoEnergy.h
+++ include/energy/picoEnergy.h
@@ -11,10 +11,10 @@
 
 #define PICOENERGY_FILENAME_VAR energymesures_filename
 
-#define PICOENERGY_DEFINE_FILENAME( BS, LC )                                            \
+#define PICOENERGY_DEFINE_FILENAME( BS, LC, RK )                                         \
     char PICOENERGY_FILENAME_VAR[ENERGY_FILENAME_LENGHT];                                \
-    sprintf(PICOENERGY_FILENAME_VAR, "%s%s_%s_%s_%d_%d.csv",                             \
-            ENERGY_PATH, ENERGY_FILENAME_PREFIX, MYBENCH_CODE, MYIMPL_CODE, BS, LC);
+    sprintf(PICOENERGY_FILENAME_VAR, "%s%s_%s_%s_%d_%d_%d.csv",                          \
+            ENERGY_PATH, ENERGY_FILENAME_PREFIX, MYBENCH_CODE, MYIMPL_CODE, BS, LC, RK);
 
 void energyCompileTimeCheck(void) {
 #if !defined(PICODCGMI) && !defined(PICONVML)
@@ -39,7 +39,7 @@ void energyCompileTimeCheck(void) {
 
 // Init profile thread for DCGMI mesurements
 #define PICODCGMI_START( BS, LC, RK )                                           \
-    PICOENERGY_DEFINE_FILENAME( BS, LC )                                        \
+    PICOENERGY_DEFINE_FILENAME( BS, LC, RK )                                    \
     std::thread threadStart;                                                    \
     dcgmiLogger dcgmi_logger ( PICOENERGY_FILENAME_VAR , RK);                   \
     if ( RK == 0 ) {                                                            \
@@ -80,25 +80,25 @@ void energyCompileTimeCheck(void) {
         unsigned long long PICONVML_ENERGYCOUNTER_START;	\
         unsigned long long PICONVML_ENERGYCOUNTER_STOP;
 
-#define PICONVML_ENERGY_START( BS, LC, DV )						                              \
-		PICOENERGY_DEFINE_FILENAME( BS, LC )					                              \
-		picoNvmlTotalEnergy(DV, &PICONVML_ENERGYCOUNTER_START);                               \
-		std::thread threadStart;                                                              \
-		myPowerSampling power_samples ( DV , PICOENERGY_FILENAME_VAR );                       \
-		threadStart = std::thread( &myPowerSampling::executePowerSampling, &power_samples );  \
+#define PICONVML_ENERGY_START( BS, LC, DV )								\
+		PICOENERGY_DEFINE_FILENAME( BS, LC, DV )					      	\
+		picoNvmlTotalEnergy(DV, &PICONVML_ENERGYCOUNTER_START);                               	\
+		std::thread threadStart;                                                              	\
+		myPowerSampling power_samples ( DV , PICOENERGY_FILENAME_VAR );                       	\
+		threadStart = std::thread( &myPowerSampling::executePowerSampling, &power_samples );  	\
 		sleep(10);
 
 
 #define PICONVML_ENERGY PICONVML_ENERGYCOUNTER_STOP - PICONVML_ENERGYCOUNTER_START
 
-#define PICONVML_ENERGY_STOP( DV )  							                \
+#define PICONVML_ENERGY_STOP( DV ) 						\
         std::thread threadKill( &myPowerSampling::killThread, &power_samples);  \
         threadStart.join( );                                                    \
         threadKill.join( );                                                     \
-		picoNvmlTotalEnergy(DV, &PICONVML_ENERGYCOUNTER_STOP);	                \
-		MPI_Barrier(MPI_COMM_WORLD);							                \
-		if (rank == 0) printf("Delta power for device:\n");		                \
-		printf("\t%i: %u\n", DV, PICONVML_ENERGY);
+	picoNvmlTotalEnergy(DV, &PICONVML_ENERGYCOUNTER_STOP);	                \
+	MPI_Barrier(MPI_COMM_WORLD);				                \
+	if (rank == 0) printf("Delta power for device:\n");		        \
+	printf("\t%i: %u\n", DV, PICONVML_ENERGY);
 
 void picoNvmlTotalEnergy(int my_dev, unsigned long long* energy) {
 
