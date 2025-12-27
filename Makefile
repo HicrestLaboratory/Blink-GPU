d747c3f11122833d0b17b98d46f3c98a1537598f
LorenzoPichetti <lori.pichi@gmail.com>
2025-12-27 05:06:51 -0500

    Update building system

---
+++ Makefile
@@ -0,0 +1,120 @@
+REQUIRED_VARS := \
+  BLINKGPU_SYSTEM \
+  BLINKGPU_CONFIGURATION \
+  BLINKGPU_SETUPDIR
+
+$(foreach v,$(REQUIRED_VARS),\
+  $(info $(v) = '$($(v))')\
+  $(if $(strip $($(v))),,\
+    $(error Environment variable $(v) is not set or empty)))
+
+
+REQUIRED_FILE := ${BLINKGPU_SETUPDIR}${BLINKGPU_SYSTEM}_${BLINKGPU_CONFIGURATION}.conf
+ifeq ($(wildcard $(REQUIRED_FILE)),)
+$(error Required file '$(REQUIRED_FILE)' does not exist)
+endif
+
+#source ${BLINKGPU_SETUPDIR}${BLINKGPU_SYSTEM}_${BLINKGPU_CONFIGURATION}.conf
+
+REQUIRED_VARS := \
+  BLINKGPU_CUDA_HOME \
+  BLINKGPU_NCCL_HOME \
+  BLINKGPU_MPI_HOME \
+  BLINKGPU_CUDA_MODULE \
+  BLINKGPU_NCCL_MODULE \
+  BLINKGPU_MPI_MODULE
+
+$(foreach v,$(REQUIRED_VARS),\
+  $(info $(v) = '$($(v))')\
+  $(if $(strip $($(v))),,\
+    $(error Environment variable $(v) is not set or empty)))
+
+# ------------------ Targets ------------------
+COMM_PATTERN  = pp a2a ar hlo mpp
+COMM_STRATEGY = Baseline CudaAware Nccl # Nvlink Nvlink temporary disable (to manage and also to change name)
+
+BINFOLDER = bin
+DIRS = $(BINFOLDER) out sout
+
+# This is expanding ALL_TARGETS = {COMM_PATTERN} x {COMM_STRATEGY}
+#  i.e. ALL_TARGETS = $(BINFOLDER)/pp_Baseline $(BINFOLDER)/pp_CudaAware ... $(BINFOLDER)/pp_Baseline ...
+define ALL_TARGETS
+$(foreach p,$(1),$(foreach s,$(2),$(BINFOLDER)/$(p)_$(s)))
+endef
+
+ALL_TARGETS_LIST := $(call ALL_TARGETS,$(COMM_PATTERN),$(COMM_STRATEGY))
+$(info ALL_TARGETS = $(ALL_TARGETS_LIST))
+
+PP_TARGETS_LIST := $(call ALL_TARGETS,pp,$(COMM_STRATEGY))
+A2A_TARGETS_LIST := $(call ALL_TARGETS,a2a,$(COMM_STRATEGY))
+AR_TARGETS_LIST := $(call ALL_TARGETS,ar,$(COMM_STRATEGY))
+$(info PP_TARGETS_LIST = $(PP_TARGETS_LIST))
+$(info A2A_TARGETS_LIST = $(A2A_TARGETS_LIST))
+$(info AR_TARGETS_LIST = $(AR_TARGETS_LIST))
+
+pp a2a ar:
+	@true
+
+all: $(ALL_TARGETS_LIST)
+pp:  $(PP_TARGETS_LIST)
+a2a: $(A2A_TARGETS_LIST)
+ar:  $(AR_TARGETS_LIST)
+
+# ------------------ Libs & Flags ------------------
+MPI=-L$(BLINKGPU_MPI_HOME)/lib -I$(BLINKGPU_MPI_HOME)/include -lmpi
+MPICUDA=-L$(BLINKGPU_MPICUDA_HOME)/lib -I$(BLINKGPU_MPICUDA_HOME)/include -lmpi
+CUDA=-L$(BLINKGPU_CUDA_HOME)/lib64 -L$(BLINKGPU_CUDA_HOME)/compact -I$(BLINKGPU_CUDA_HOME)/include -lcudart -lcuda
+NCCL=-L$(BLINKGPU_NCCL_HOME)/lib -I$(BLINKGPU_NCCL_HOME)/include -lnccl
+
+BASELINE_LIBS = $(MPI) $(CUDA)
+CUDAAWARE_LIBS = $(MPICUDA) $(CUDA)
+NCCL_LIBS = $(MPI) $(CUDA) $(NCCL)
+NVLINK_LIBS = $(MPI) $(CUDA)
+
+# ------------------ Modules ------------------
+BASELINE_MODULES = $(BLINKGPU_CUDA_MODULE) $(BLINKGPU_MPI_MODULE)
+CUDAAWARE_MODULES = $(BLINKGPU_CUDA_MODULE) $(BLINKGPU_MPICUDA_MODULE)
+NCCL_MODULES = $(BLINKGPU_CUDA_MODULE) $(BLINKGPU_NCCL_MODULE)
+
+MODULEFOLDER=module_dir/
+BASELINE_MODULE_FILE=${MODULEFOLDER}baseline.mod
+CUDAAWARE_MODULE_FILE=${MODULEFOLDER}cudaaware.mod
+NCCL_MODULE_FILE=${MODULEFOLDER}nccl.mod
+
+$(BASELINE_MODULE_FILE):
+	mkdir -p ${MODULEFOLDER}
+	@echo "module purge" > $@
+	@echo "module load $(BASELINE_MODULES)" >> $@
+
+$(CUDAAWARE_MODULE_FILE):
+	mkdir -p ${MODULEFOLDER}
+	@echo "module purge" > $@
+	@echo "module load $(CUDAAWARE_MODULES)" >> $@
+
+$(NCCL_MODULE_FILE):
+	mkdir -p ${MODULEFOLDER}
+	@echo "module purge" > $@
+	@echo "module load $(NCCL_MODULES)" >> $@
+
+# ------------------  Rules  ------------------
+CC=$(BLINKGPU_CUDA_HOME)/bin/nvcc
+# COMPILE = $(CC) $(CFLAGS) $(INCL) $(LIBS) $(LIBFLAGS) \
+#           -lstdc++ -lm -Wno-deprecated-gpu-targets $(DBGFLAGS)
+
+$(BINFOLDER)/%_Baseline: src/%_Baseline.cu $(BASELINE_MODULE_FILE) | $(DIRS)
+	@echo "Building $@"
+	mkdir -p ${BINFOLDER}
+	source $(BASELINE_MODULE_FILE) && $(CC) $(CFLAGS) -o $@ $< $(BASELINE_LIBS)
+
+$(BINFOLDER)/%_CudaAware: src/%_CudaAware.cu $(CUDAAWARE_MODULE_FILE) | $(DIRS)
+	@echo "Building $@"
+	mkdir -p ${BINFOLDER}
+	source $(CUDAAWARE_MODULE_FILE) && $(CC) $(CFLAGS) -o $@ $< $(CUDAAWARE_LIBS)
+
+$(BINFOLDER)/%_Nccl: src/%_Nccl.cu $(NCCL_MODULE_FILE) | $(DIRS)
+	@echo "Building $@"
+	mkdir -p ${BINFOLDER}
+	source $(NCCL_MODULE_FILE) && $(CC) $(CFLAGS) -o $@ $< $(NCCL_LIBS)
+
+clean:
+	rm -rf $(BINFOLDER)/*
---
+++ init.sh
@@ -0,0 +1,179 @@
+#!/bin/bash
+
+# --- Read inputr params ---
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+usage() {
+  cat <<EOF
+Usage: $0 --system <name> [--configuration <conf>]
+
+Options:
+  --system         (required) System name
+  --configuration  (optional) Configuration name
+  -h, --help       Show this help message
+EOF
+  exit 1
+}
+
+# Parse arguments
+PARSED_ARGS=$(getopt -o h --long help,system:,configuration: -- "$@") || usage
+eval set -- "$PARSED_ARGS"
+
+SYSTEM=""
+CONFIGURATION=""
+
+while true; do
+  case "$1" in
+    --system)
+      SYSTEM="${2^^}"
+      shift 2
+      ;;
+    --configuration)
+      CONFIGURATION="${2^^}"
+      shift 2
+      ;;
+    -h|--help)
+      usage
+      ;;
+    --)
+      shift
+      break
+      ;;
+    *)
+      echo "Unexpected option: $1"
+      usage
+      ;;
+  esac
+done
+
+# Validate mandatory parameter
+if [[ -z "$SYSTEM" ]]; then
+  echo "Error: --system is required."
+  usage
+fi
+
+# ---- Script logic below ----
+echo "System: $SYSTEM"
+
+if [[ -n "$CONFIGURATION" ]]; then
+  echo "Configuration: $CONFIGURATION"
+else
+  CONFIGURATION="DEFAULT"
+  echo "Configuration: $CONFIGURATION"
+fi
+
+#export BLINKGPU_SYSTEM=${SYSTEM}
+#export BLINKGPU_CONFIGURATION=${CONFIGURATION}
+#export BLINKGPU_SETUPDIR="setup_dir/"
+# ---------------------------------------
+
+dt=$(date '+%d/%m/%Y %H:%M:%S');
+echo "$dt"
+
+write_setup_file() {
+  local setupfile="$1"
+  local cuda_home="$2"
+  local nccl_home="$3"
+  local mpi_home="$4"
+  local mpicuda_home="$5"
+  local cuda_module="$6"
+  local nccl_module="$7"
+  local mpi_module="$8"
+  local mpicuda_module="$9"
+
+  {
+    echo "#!/bin/bash"
+    echo "# Generated on $dt"
+    echo
+    echo "export BLINKGPU_SYSTEM=${SYSTEM}"
+    echo "export BLINKGPU_CONFIGURATION=${CONFIGURATION}"
+    echo "export BLINKGPU_SETUPDIR=${SETUP_DIR}"
+    echo
+    echo "export BLINKGPU_CUDA_HOME=\"${cuda_home}\""
+    echo "export BLINKGPU_NCCL_HOME=\"${nccl_home}\""
+    echo "export BLINKGPU_MPI_HOME=\"${mpi_home}\""
+    echo "export BLINKGPU_MPICUDA_HOME=\"${mpicuda_home}\""
+    echo
+    echo "export BLINKGPU_CUDA_MODULE=\"${cuda_module}\""
+    echo "export BLINKGPU_NCCL_MODULE=\"${nccl_module}\""
+    echo "export BLINKGPU_MPI_MODULE=\"${mpi_module}\""
+    echo "export BLINKGPU_MPICUDA_MODULE=\"${mpicuda_module}\""
+  } > "$setupfile"
+}
+
+
+# =========== Variables to export ===========
+# Home variables:   BLINKGPU_CUDA_HOME,   BLINKGPU_NCCL_HOME,   BLINKGPU_MPI_HOME
+# Module variables: BLINKGPU_CUDA_MODULE, BLINKGPU_NCCL_MODULE, BLINKGPU_MPI_MODULE
+# ===========================================
+
+SETUP_DIR="configure/"
+setupfile="${SETUP_DIR}${SYSTEM}_${CONFIGURATION}.conf"
+mkdir -p ${SETUP_DIR}
+case "${SYSTEM}:${CONFIGURATION}" in
+  BALDO:DEFAULT)
+    echo "Generating exports for ${SYSTEM} system and ${CONFIGURATION} configuration"
+    # Home directories
+    cuda_home="/opt/shares/cuda/software/CUDA/12.3.2"
+    nccl_home="/opt/shares/NVHPC/nvhpc_24.7_cuda_12.5/Linux_x86_64/24.7/comm_libs/12.5/nccl"
+    mpi_home="/opt/shares/openmpi-4.1.5-cuda-12.3.2"
+    mpicuda_home="/opt/shares/openmpi-4.1.5-cuda-12.3.2"
+
+    # Module name
+    cuda_module="CUDA/12.3.2"
+    nccl_module="NVHPC/nvhpc/24.7"
+    mpi_module="OpenMpi/4.1.5-CUDA-12.3.2"
+    mpicuda_module="OpenMpi/4.1.5-CUDA-12.3.2"
+    ;;
+
+  BALDO:NVHPC)
+    echo "Generating exports for ${SYSTEM} system and ${CONFIGURATION} configuration"
+    # Home directories
+    cuda_home="/opt/shares/NVHPC/nvhpc_24.7_cuda_12.5/Linux_x86_64/24.7/cuda"
+    nccl_home="/opt/shares/NVHPC/nvhpc_24.7_cuda_12.5/Linux_x86_64/24.7/comm_libs/12.5/nccl"
+    mpi_home="/opt/shares/NVHPC/nvhpc_24.7_cuda_12.5/Linux_x86_64/24.7/comm_libs/12.5/openmpi4/openmpi-4.1.5"
+    mpicuda_home="/opt/shares/NVHPC/nvhpc_24.7_cuda_12.5/Linux_x86_64/24.7/comm_libs/12.5/openmpi4/openmpi-4.1.5"
+
+    # Module name
+    cuda_module="NVHPC/nvhpc/24.7"
+    nccl_module="NVHPC/nvhpc/24.7"
+    mpi_module="NVHPC/nvhpc/24.7"
+    mpicuda_module="NVHPC/nvhpc/24.7"
+    ;;
+
+  LEONARDO:DEFAULT)
+    echo "Generating exports for ${SYSTEM} system and ${CONFIGURATION} configuration"
+    
+    # Home directories
+    cuda_home="/leonardo/prod/spack/06/install/0.22/linux-rhel8-icelake/gcc-8.5.0/nvhpc-24.5-torlmnyzcexnrs6pq4cccabv7ehkv3xy/Linux_x86_64/24.5/cuda"
+    nccl_home="/leonardo/prod/spack/06/install/0.22/linux-rhel8-icelake/gcc-8.5.0/nvhpc-24.5-torlmnyzcexnrs6pq4cccabv7ehkv3xy/Linux_x86_64/24.5/comm_libs/nccl"
+    mpi_home="/leonardo/prod/spack/06/install/0.22/linux-rhel8-icelake/gcc-8.5.0/nvhpc-24.5-torlmnyzcexnrs6pq4cccabv7ehkv3xy/Linux_x86_64/24.5/comm_libs/openmpi/openmpi-3.1.5"
+    mpicuda_home="/leonardo/prod/spack/06/install/0.22/linux-rhel8-icelake/gcc-8.5.0/nvhpc-24.5-torlmnyzcexnrs6pq4cccabv7ehkv3xy/Linux_x86_64/24.5/comm_libs/openmpi/openmpi-3.1.5"
+
+    # Module name
+    cuda_module="nvhpc/24.5"
+    nccl_module="nvhpc/24.5"
+    mpi_module="nvhpc/24.5"
+    mpicuda_module="nvhpc/24.5"    
+    ;;
+
+  *)
+    echo "Error: Unknown system '$SYSTEM'"
+    exit 1
+    ;;
+esac
+
+write_setup_file \
+      "$setupfile" \
+      "$cuda_home" \
+      "$nccl_home" \
+      "$mpi_home" \
+      "$mpicuda_home"\
+      "$cuda_module" \
+      "$nccl_module" \
+      "$mpi_module" \
+      "$mpicuda_module"
+
+#source ${BLINKGPU_SETUPDIR}${BLINKGPU_SYSTEM}_${BLINKGPU_CONFIGURATION}.conf
